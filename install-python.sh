f51cb1eef2483dc935a5c6869ffd867693a553c8
Kartikay Bagla <kartikaybagla@gmail.com>
2021-07-26 17:13:31 +0530

    Added script to install python

---
+++ install-python.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Call this script as sudo install-python.sh <version-number>
+# Example: sudo install-python.sh 3.7.10
+apt-get update
+apt-get install build-essential zlib1g-dev libncursesw5-dev libssl-dev \
+    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libreadline-dev \
+    libnss3-dev libffi-dev wget
+
+wget https://www.python.org/ftp/python/$1/Python-$1.tgz
+tar -xvzf Python-$1.tgz
+cd Python-$1
+
+./configure
+make altinstall
+
+cd ..
+rm -rf Python-$1
+rm Python-$1.tgz
