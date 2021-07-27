d8fd409a021a9be9b4ea63646654142d51a91a47
Kartikay Bagla <kartikaybagla@gmail.com>
2021-07-27 11:33:47 +0530

    updated python script

--- install-python.sh
+++ install-python.sh
@@ -4,15 +4,15 @@ set -e
 # Call this script as sudo install-python.sh <version-number>
 # Example: sudo install-python.sh 3.7.10
 apt-get update
-apt-get install build-essential zlib1g-dev libncursesw5-dev libssl-dev \
+apt-get install -y build-essential zlib1g-dev libncursesw5-dev libssl-dev \
     libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libreadline-dev \
-    libnss3-dev libffi-dev wget
+    libnss3-dev libffi-dev liblzma-dev lzma wget
 
 wget https://www.python.org/ftp/python/$1/Python-$1.tgz
 tar -xvzf Python-$1.tgz
 cd Python-$1
 
-./configure
+./configure --enable-optimizations
 make altinstall
 
 cd ..
